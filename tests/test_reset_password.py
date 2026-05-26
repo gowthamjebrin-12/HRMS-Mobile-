971a14fe771fa94808c91c40d54f88659a09cfa6
gowtham_jebrin <gowtham.jebrin@tringapps.net>
2026-05-26 16:00:39 +0530

    unused

--- tests/test_reset_password.py
+++
@@ -1,7 +0,0 @@
-from pages.login.reset_password import ResetPassword
-
-def test_reset_password(driver):
-    reset_password_page = ResetPassword(driver)
-    reset_password_page.reset_password("User@123", "NewUser@123")
-
-    print("Password reset successful")
--- tests/test_sidebar_nav.py
+++
@@ -1,8 +0,0 @@
-from pages.sidebar_nav.sidebar_nav import SidebarNav
-
-def test_sidebar_navigation(driver):
-    
-    sidebar_nav_page = SidebarNav(driver)
-    sidebar_nav_page.sidebar_navigation()
-
-    print("Sidebar navigation successful")
